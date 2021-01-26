717e20e44635f5d4141de235c8dca8ab982df9f1
sandeeppowar <sandeep122powar@gmail.com>
2021-01-26 00:05:35 -0500

    2 tests

---
+++ src/main/java/com/qa/hubspot/pages/Pages.java
@@ -0,0 +1,31 @@
+package com.qa.hubspot.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Pages {
+
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    //Page class constructor
+    public Pages(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+    }
+
+    //java generics and return new page
+
+    public <TPage extends BasePage> TPage getInstance(Class<TPage> pageClass) {
+        try {
+            return pageClass.getDeclaredConstructor(WebDriver.class, WebDriverWait.class).newInstance(this.driver, this.wait);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+
+    }
+
+
+}
